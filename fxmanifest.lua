1506c0037221100b4a3261fc5a9c59db7b020592
Daemon Brooks <54357300+xotikorukx@users.noreply.github.com>
2023-12-05 10:42:35 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 version '1.0.0'
 
-author 'Daemon Brooks <presortstd@dbrooks.email>'
+author 'Daemon Brooks <presortstd@dbrooks.email> xotikorukx#4064'
 
 client_scripts {
     'firstspawn.lua'
